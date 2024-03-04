87920cca5d6ec52d31bace11d2c1f508e67690db
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-03-03 21:47:07 -0400

    commit

---
+++ index.js
@@ -0,0 +1,33 @@
+class Usuarios{
+    constructor(usuarioId, usuarioNickname, usuarioNome){
+        this.usuarioId = usuarioId;
+        this.usuarioNickName = usuarioNickname;
+        this.usuarioNome = usuarioNome;
+    }
+}
+
+class Jogos{
+    constructor(jogoId, titulo, descricao, precoBase){
+        this.jogoId = jogoId;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.precoBase = precoBase;
+    }
+}
+
+class Conquista{
+    constructor(conquistaId, conquistTitulo,conquistaDescricao){
+        this.conquistaId = conquistaId;
+        this.conquistaTitulo = conquistTitulo;
+        this.conquistaDescricao = conquistaDescricao;
+    }
+}
+
+const usuario = new Usuarios(1,'VieiraBeh', 'Brenda Vieira');
+console.log(usuario);
+
+const jogo = new Jogos(2, 'League of legends', 'Destrua as torres!', 20);
+console.log(jogo);
+
+const conquista = new Conquista(3, 'MVP 3','Terceira conquista', 'Parabéns pela conaquista');
+console.log(conquista);
